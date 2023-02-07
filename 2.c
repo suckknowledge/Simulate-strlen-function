d451643bb33ed9fa802f0965e6946771cf1d3266
suckknowledge <124077161+suckknowledge@users.noreply.github.com>
2023-02-07 23:35:24 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
-#include<assert.h>
-size_t my_strlen(const char* arr[])//因为长度不可能是负数，所以用无符号整形 ，size_t 表示unsigned int
-{
-	assert(arr);
-	size_t count = 0;
-	while (*arr++)
-	{
-		count++;
-	}
-	return count;
-}
 int main()
 {
-	int a = 0;
-	char arr[] = "1233334";
-	a = my_strlen(arr);
-	printf("%d", a);
+	int a = 10;
+	int b = 20;
+	printf("%d  %d\n", a, b);
+	a = a ^ b;
+	b = a ^ b;
+	a = b ^ a;
+	printf("%d  %d", a, b);
 	return 0;
 }
